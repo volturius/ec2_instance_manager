78d7cf6c89e2a939d971267fee6bc675735b2020
Tom McDonald <tom@volturius.com>
2013-09-22 22:23:10 -0700

    improved instance list logic

--- ec2admin.py
+++ ec2admin.py
@@ -15,32 +15,36 @@ class ec2admin(object):
     def add_tag(self, name, instance):
         instance.add_tag("Name","{{INSERT NAME}}")
 
-    def get_instance_names(self):
+    def get_instance_names(self, instance_list):
 
-        conn = boto.ec2.connect_to_region(self.region.name)
+        conn = boto.ec2.connect_to_region(self.region)
 
         reservations = conn.get_all_instances()
 
         for reservation in reservations:
-            print reservation
+            
+#            print reservation
             instances = reservation.instances
 
-            print instances
+#            print instances
 
             # instance attributes
             #   ['__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_in_monitoring_element', '_placement', '_previous_state', '_state', '_update', 'add_tag', 'ami_launch_index', 'architecture', 'block_device_mapping', 'client_token', 'confirm_product', 'connection', 'create_image', 'dns_name', 'ebs_optimized', 'endElement', 'eventsSet', 'get_attribute', 'get_console_output', 'group_name', 'groups', 'hypervisor', 'id', 'image_id', 'instance_profile', 'instance_type', 'interfaces', 'ip_address', 'item', 'kernel', 'key_name', 'launch_time', 'modify_attribute', 'monitor', 'monitored', 'monitoring', 'monitoring_state', 'persistent', 'placement', 'placement_group', 'placement_tenancy', 'platform', 'previous_state', 'previous_state_code', 'private_dns_name', 'private_ip_address', 'product_codes', 'public_dns_name', 'ramdisk', 'reason', 'reboot', 'region', 'remove_tag', 'requester_id', 'reset_attribute', 'root_device_name', 'root_device_type', 'spot_instance_request_id', 'start', 'startElement', 'state', 'state_code', 'state_reason', 'stop', 'subnet_id', 'tags', 'terminate', 'unmonitor', 'update', 'use_ip', 'virtualization_type', 'vpc_id']
 
             for inst in instances:
-                print str(inst) + " ",
-                print "'%s'" % inst.tags['Name']
-                print "\t%s" % inst.state
-                print "\t%s" % inst.instance_type
-                print "\t%s" % inst.placement
-                print "\t%s" % inst.key_name
-                print "\t%s" % inst.dns_name
-                print "\t%s" % inst.private_ip_address
-                print "\t%s" % inst.public_dns_name
-                print "\t%s" % inst.ip_address
+
+                if ((instance_list == None) or (inst.tags['Name'] in instance_list)):
+
+                    print str(inst) + " ",
+                    print "'%s'" % inst.tags['Name']
+                    print "\t%s" % inst.state
+                    print "\t%s" % inst.instance_type
+                    print "\t%s" % inst.placement
+                    print "\t%s" % inst.key_name
+                    print "\t%s" % inst.dns_name
+                    print "\t%s" % inst.private_ip_address
+                    print "\t%s" % inst.public_dns_name
+                    print "\t%s" % inst.ip_address
 #            print "\t%s" % inst.get_console_output().output
 
 
@@ -59,12 +63,11 @@ def main(argv=None):
     subparsers = parser.add_subparsers()
 
     # create the parser for the "list" command
-    # NOTE: nargs='?' means argument is optional
     parser_start = subparsers.add_parser('list', help='start specified instance')
-    parser_start.add_argument('list_region', default='ALL', nargs='?', help='instance help')
+    parser_start.add_argument('list_regions', default='ALL', nargs='*', help='instance help')
+    parser_start.add_argument('--instances', nargs='*', help='instance help')
 
     # create the parser for the "start" command
-    # NOTE: no nargs means argument is required
     parser_start = subparsers.add_parser('start', help='start specified instance')
     parser_start.add_argument('start_instance', default=None, metavar='ID', help='instance help')
 
@@ -78,15 +81,29 @@ def main(argv=None):
 
     args = parser.parse_args()
 
-    if hasattr(args, 'list_region'):
-        print "listing region %s" % args.list_region
-        regions = boto.ec2.regions()
+    if hasattr(args, 'list_regions'):
+
+        ec2_regions = [region.name for region in boto.ec2.regions()]
+
+        instances = args.instances
+        regions = args.list_regions
+
+        if (regions == 'ALL'):
+            regions = ec2_regions
+
+        print "listing region %s" % regions
+
         for region in regions:
+
+            if region not in ec2_regions:
+                print "WARNING: bad region specified '%s'" % region
+                continue
+
             print "getting region '%s'" % region
 
             ec2a = ec2admin(region)
 
-            ec2a.get_instance_names()
+            ec2a.get_instance_names(instances)
             print
 
     if hasattr(args, 'stop_instance'):
